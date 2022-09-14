e6860afa16e1faf2f54e73f36c9a65f161ea4f82
Nancy-GDA <nancy.glez.dea@gmail.com>
2022-09-13 20:55:21 -0500

    Evento de click en los botones

--- index.js
+++ index.js
@@ -1,111 +1,72 @@
-var operandoa;
-var operandob;
-var operacion;
-function init(){
-    //variables
-    var resultado = document.getElementById('resultado');
-    var reset = document.getElementById('reset');
-    var suma = document.getElementById('suma');
-    var resta = document.getElementById('resta');
-    var multiplicacion = document.getElementById('multiplicacion');
-    var division = document.getElementById('division');
-    var igual = document.getElementById('igual');
-    var uno = document.getElementById('uno');
-    var dos = document.getElementById('dos');
-    var tres = document.getElementById('tres');
-    var cuatro = document.getElementById('cuatro');
-    var cinco = document.getElementById('cinco');
-    var seis = document.getElementById('seis');
-    var siete = document.getElementById('siete');
-    var ocho = document.getElementById('ocho');
-    var nueve = document.getElementById('nueve');
-    var cero = document.getElementById('cero');
-  }
+const operator = document.querySelectorAll('.operator')
+const ouputValue = document.querySelector('.output-value')
+const textDisplay = document.querySelector(".text-display");
+const one = document.querySelector(".one");
+const two = document.querySelector(".two");
+const three = document.querySelector(".three");
+const four = document.querySelector(".four");
+const five = document.querySelector(".five");
+const six = document.querySelector(".six");
+const seven = document.querySelector(".seven");
+const eigth = document.querySelector(".eigth");
+const nine = document.querySelector(".nine");
+const zero = document.querySelector('.zero')
+const remain = document.querySelector('.remain')
+const sumary = document.querySelector('.sumary-sing')
+const multiplication = document.querySelector('.multiplication')
+const division = document.querySelector('.division')
+const point = document.querySelector('.point')
+const clean = document.querySelector('.clean')
 
-  //Eventos de click
-  uno.onclick = function(e){
-    resultado.textContent = resultado.textContent  + "1";
-}
-dos.onclick = function(e){
-    resultado.textContent = resultado.textContent  + "2";
-}
-tres.onclick = function(e){
-    resultado.textContent = resultado.textContent  + "3";
-}
-cuatro.onclick = function(e){
-    resultado.textContent = resultado.textContent  + "4";
-}
-cinco.onclick = function(e){
-    resultado.textContent = resultado.textContent  + "5";
-}
-seis.onclick = function(e){
-    resultado.textContent = resultado.textContent  + "6";
-}
-siete.onclick = function(e){
-    resultado.textContent = resultado.textContent  + "7";
-}
-ocho.onclick = function(e){
-    resultado.textContent = resultado.textContent  + "8";
-}
-nueve.onclick = function(e){
-    resultado.textContent = resultado.textContent  + "9";
-}
-cero.onclick = function(e){
-    resultado.textContent = resultado.textContent  + "0";
-}
-reset.onclick = function(e){
-    resetear();
-}
-suma.onclick = function(e){
-    operandoa = resultado.textContent;
-    operacion = "+";
-    limpiar();
-}
-resta.onclick = function(e){
-    operandoa = resultado.textContent;
-    operacion = "-";
-    limpiar();
-}
-multiplicacion.onclick = function(e){
-    operandoa = resultado.textContent;
-    operacion = "*";
-    limpiar();
-}
-division.onclick = function(e){
-    operandoa = resultado.textContent;
-    operacion = "/";
-    limpiar();
-}
-igual.onclick = function(e){
-    operandob = resultado.textContent;
-    resolver();
-}
-function limpiar(){
-    resultado.textContent = "";
-  }
-  function resetear(){
-    resultado.textContent = "";
-    operandoa = 0;
-    operandob = 0;
-    operacion = "";
-  }
-  
-  function resolver(){
-    var res = 0;
-    switch(operacion){
-      case "+":
-        res = parseFloat(operandoa) + parseFloat(operandob);
-        break;
-      case "-":
-          res = parseFloat(operandoa) - parseFloat(operandob);
-          break;
-      case "*":
-        res = parseFloat(operandoa) * parseFloat(operandob);
-        break;
-      case "/":
-        res = parseFloat(operandoa) / parseFloat(operandob);
-        break;
-    }
-    resetear();
-    resultado.textContent = res;
-  }
+one.addEventListener("click", () => {
+  textDisplay.innerText = "1";
+});
+two.addEventListener("click", () => {
+  textDisplay.innerText = "2";
+});
+three.addEventListener("click", () => {
+  textDisplay.innerText = "3";
+});
+four.addEventListener("click", () => {
+  textDisplay.innerText = "4";
+});
+five.addEventListener("click", () => {
+  textDisplay.innerText = "5";
+});
+six.addEventListener("click", () => {
+  textDisplay.innerText = "6";
+});
+seven.addEventListener("click", () => {
+  textDisplay.innerText = "7";
+});
+eigth.addEventListener("click", () => {
+  textDisplay.innerText = "8";
+});
+nine.addEventListener("click", () => {
+  textDisplay.innerText = "9";
+});
+
+zero.addEventListener('click',() => {
+    textDisplay.innerText = "0";
+})
+
+point.addEventListener('click',() =>{
+    textDisplay.innerText = '.'
+})
+
+
+remain.addEventListener('click',() =>{
+    ouputValue.innerText = '-'
+})
+
+sumary.addEventListener('click',() =>{
+    ouputValue.innerText = '+'
+})
+
+division.addEventListener('click',() =>{
+    ouputValue.innerText = '/'
+})
+
+multiplication.addEventListener('click',() =>{
+    ouputValue.innerText = 'X'
+})
